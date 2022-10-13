9d9de27a13be1a896c8decff45e5ed05dcaa80b5
gsnorberto <sn_gabriel@outlook.com>
2022-10-13 12:56:25 -0300

    Inserido a função de enviar mensagem para o WhatsApp

--- script.js
+++ script.js
@@ -2,21 +2,35 @@ const ad = document.querySelector('.main-course .ad');
 
 const clicked = (category, price, id) => {
     const adSelected = document.querySelector(`.${category} .ad.selected`);
-    
 
-    if(adSelected){ // tem elemento selecionado na categoria
+
+    if (adSelected) { // tem elemento selecionado na categoria
         adSelected.classList.remove('selected');
-    } 
+    }
 
     document.getElementById(id).classList.add('selected');
 
     const selected_items = (document.querySelectorAll('.selected').length);
-    if(selected_items == 3){
+    if (selected_items == 3) {
         document.querySelector('.button_footer').style.backgroundColor = '#3EB503';
         document.querySelector('.button_footer').removeAttribute('disabled');
     }
 }
 
 const review_order = () => {
-    console.log('ok');
+    let foodName = 'comida teste';
+    let drinkName = 'Bebida teste';
+    let dessertName = 'Sobremesa teste';
+    let price = '12.50';
+
+    let text = encodeURIComponent(`
+        Olá, gostaria de fazer o pedido:
+        - Prato: ${foodName}
+        - Bebida: ${drinkName}
+        - Sobremesa: ${dessertName}
+        Total: R$ ${price}
+    `);
+
+    let link = 'https://wa.me/5573988983200?text='+text;
+    document.querySelector('.button_footer a').setAttribute('href', link);
 }
