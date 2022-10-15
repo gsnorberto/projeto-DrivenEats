ab9fc011c17f62f48e25b9908b07d125b89123f7
gsnorberto <sn_gabriel@outlook.com>
2022-10-15 09:17:51 -0300

    Caixa de confirmação implementada

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let selected_items = [];
+
+let foodInfoArray = [];
+let drinkInfoArray = [];
+let dessertInfoArray = [];
+
+let price1 = 0;
+let price2 = 0;
+let price3 = 0;
+let totalPrice = 0;
+
 const clicked = (category, price, id) => {
     const adSelected = document.querySelector(`.${category} .ad.selected`);
 
@@ -16,29 +27,46 @@ const clicked = (category, price, id) => {
 }
 
 const review_order = () => {
-    const selected_items = document.querySelectorAll('.selected');
-    console.log(selected_items);
+    selected_items = document.querySelectorAll('.selected');
 
-    let foodInfoArray = (selected_items[0].innerText).split("\n");
-    let drinkInfoArray = (selected_items[1].innerText).split("\n");
-    let dessertInfoArray = (selected_items[2].innerText).split("\n");
+    // Get item information: title, description and price
+    foodInfoArray = (selected_items[0].innerText).split("\n");
+    drinkInfoArray = (selected_items[1].innerText).split("\n");
+    dessertInfoArray = (selected_items[2].innerText).split("\n");
 
-    const price1 = Number(foodInfoArray[2].replace("R$ " , "").replace(',' , "."));
-    const price2 = Number(drinkInfoArray[2].replace("R$ " , "").replace(',' , "."));
-    const price3 = Number(dessertInfoArray[2].replace("R$ " , "").replace(',' , "."));
-    console.log(price1+price2+price3);
-    
+    // price formatting - R$ 2,00 -> 2.00
+    price1 = Number(foodInfoArray[2].replace("R$ ", "").replace(',', ".")).toFixed(2);
+    price2 = Number(drinkInfoArray[2].replace("R$ ", "").replace(',', ".")).toFixed(2);
+    price3 = Number(dessertInfoArray[2].replace("R$ ", "").replace(',', ".")).toFixed(2);
 
-    let price = (price1 + price2 + price3).toFixed(2);
+    //console.log(typeof(price1));
+    totalPrice = (Number(price1) + Number(price2) + Number(price3)).toFixed(2);
 
+    // insert food name and price in confirmation box
+    document.querySelector('.item-1 .item-title').innerHTML = `${foodInfoArray[0]}`;
+    document.querySelector('.item-1 .item-price').innerHTML = price1.toString().replace('.', ',');
+    document.querySelector('.item-2 .item-title').innerHTML = `${drinkInfoArray[0]}`;
+    document.querySelector('.item-2 .item-price').innerHTML = price2.toString().replace('.', ',');
+    document.querySelector('.item-3 .item-title').innerHTML = `${dessertInfoArray[0]}`;
+    document.querySelector('.item-3 .item-price').innerHTML = price3.toString().replace('.', ',');
+    document.querySelector('.order-total-price .item-price').innerHTML = `R$ ${totalPrice.toString().replace('.', ',')}`;
+
+    document.querySelector('.box-background').style.display = 'block';
+}
+
+const confirm_order = () => {
     let text = encodeURIComponent(`
         Olá, gostaria de fazer o pedido:
         - Prato: ${foodInfoArray[0]}
         - Bebida: ${drinkInfoArray[0]}
         - Sobremesa: ${dessertInfoArray[0]}
-        Total: R$ ${price}
+        Total: R$ ${totalPrice}
     `);
 
-    let link = 'https://wa.me/5573988983200?text='+text;
-    document.querySelector('.button_footer a').setAttribute('href', link);
+    let link = 'https://wa.me/5573988983200?text=' + text;
+    document.querySelector('.button_box a').setAttribute('href', link);
+}
+
+const close_box = () => {
+    document.querySelector('.box-background').style.display = 'none';
 }
